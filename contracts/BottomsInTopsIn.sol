ec552a8a9fd801a799e83e4c8fdb18dbdb4f7de0
shitcoinoor.eth <alkarimweb3@gmail.com>
2024-07-11 01:06:14 -0400

    chainlink-import-typo-fix

--- contracts/BottomsInTopsIn.sol
+++ contracts/BottomsInTopsIn.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@chainlink/contracts/";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/structs/Checkpoints.sol";
